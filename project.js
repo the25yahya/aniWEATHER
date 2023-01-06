01a341c59fa0aea7ed9da951362236af99805271
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-01-06 21:20:39 +0100

    observations data , main body data

--- project.js
+++ project.js
@@ -1,4 +1,10 @@
 //variables 
+const maindesc = document.getElementById('widget-desc-info');
+const sunset = document.getElementById('sunset-info');
+const sunrise = document.getElementById('sunrise-info');
+const humidity = document.getElementById('humidity-info');
+const visibility = document.getElementById('visibility-info');
+const pressure = document.getElementById('pressure-info');
 const input = document.querySelector('.input');
 const search = document.querySelector('.search');
 const body = document.querySelector('.main-content');
@@ -52,6 +58,40 @@ function bodyData(){
     console.log(data);
     widgetTemp.innerText= data.main.temp+'°c';
     widgetWind.innerText = 'wind : '+ data.wind.speed + 'm/s'
+    humidity.innerHTML=data.main.humidity+'%';
+    maindesc.innerHTML=data.weather[0].description
+    pressure.innerHTML=data.main.pressure;
+    visibility.innerHTML=data.visibility+'mb';
+    let sunrisemiliseconds = data.sys.sunrise;
+    let sunriseDate = new Date(sunrisemiliseconds);
+    let hours = sunriseDate.getHours();
+    let minutes = sunriseDate.getMinutes();
+    let time_string = hours + ':' + minutes;
+    if(minutes<10){
+      time_string += minutes;
+    }
+    if (hours >= 12){
+      time_string += " PM";
+    }else{
+      time_string += " AM"
+    }
+    sunrise.innerHTML=time_string;
+    let sunsetmiliseconds = data.sys.sunset;
+    console.log(sunsetmiliseconds)
+    let sunsetDate = new Date(sunsetmiliseconds);
+    let hours2 = sunriseDate.getHours();
+    let minutes2 = sunriseDate.getMinutes();
+    let time_string2 = hours2 + ':' + minutes2;
+    if(minutes2<10){
+      time_string2 += minutes2;
+    }
+    if (hours2 >= 12){
+      time_string2 += " PM";
+    }else{
+      time_string2 += " AM"
+    }
+     sunset.innerHTML=time_string2;
+
 
   })
 
